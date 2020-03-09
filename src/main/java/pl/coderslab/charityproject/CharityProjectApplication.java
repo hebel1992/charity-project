22ee3f41fc67f983f7ec5a9ccc320ac2df2223f1
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-09 22:02:12 +0100

    landing page view and controller set up

---
+++ src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.charityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/charityproject/CharityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.charityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
