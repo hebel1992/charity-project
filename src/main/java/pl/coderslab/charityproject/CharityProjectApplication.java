b3fccd4163f3b0684495e16aacb0d52bc6154119
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-16 20:39:12 +0100

    utf-8 antoher fix

--- src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
+++ src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
@@ -2,16 +2,12 @@ package pl.coderslab.charityproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
-
-import java.lang.reflect.Field;
-import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class CharityProjectApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CharityProjectApplication.class, args);
     }
--- src/main/java/pl/coderslab/charityproject/EncodingFilter.java
+++ src/main/java/pl/coderslab/charityproject/EncodingFilter.java
@@ -1,23 +1,20 @@
 package pl.coderslab.charityproject;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-
-@Component
-public class EncodingFilter {
-
-    @Autowired
-    public EncodingFilter(ServletContext container) {
-
-        FilterRegistration.Dynamic fr = container.addFilter("encodingFilter",
-                new CharacterEncodingFilter());
-        fr.setInitParameter("encoding", "UTF-8");
-        fr.setInitParameter("forceEncoding", "true");
-        fr.addMappingForUrlPatterns(null, true, "/*");
+@Configuration
+public class EncodingFilter extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
+        //rest of your code
     }
-
+//rest of your code
 }
