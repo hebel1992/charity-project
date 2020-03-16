65e6c00f62d8f4e13dac10316ffa28b0dd16842c
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-16 19:52:40 +0100

    utf-8 antoher fix

--- src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
+++ src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
@@ -7,9 +7,22 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+
 @SpringBootApplication
 public class CharityProjectApplication {
     public static void main(String[] args) {
+        System.setProperty("file.encoding","UTF-8");
+        Field charset = null;
+        try {
+            charset = Charset.class.getDeclaredField("defaultCharset");
+            charset.setAccessible(true);
+            charset.set(null,null);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
         SpringApplication.run(CharityProjectApplication.class, args);
     }
 
