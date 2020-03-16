fd413d3892da1e705e0c5e122a253aae9fbe1d88
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-16 17:38:36 +0100

    encoding UTF-8 set

--- src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
+++ src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
@@ -2,9 +2,22 @@ package pl.coderslab.charityproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class CharityProjectApplication {
+    @Bean
+    @Primary
+    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new ClassPathResource("app.properties"));
+        configurer.setFileEncoding("UTF-8");
+        return configurer;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CharityProjectApplication.class, args);
     }
