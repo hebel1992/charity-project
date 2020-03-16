2dfc4d781df469a32f8c1f2b379439dc003f768c
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-16 20:41:02 +0100

    utf-8 antoher fix

--- src/main/java/pl/coderslab/charityproject/EncodingFilter.java
+++ src/main/java/pl/coderslab/charityproject/EncodingFilter.java
@@ -8,13 +8,6 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 public class EncodingFilter extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        http.addFilterBefore(filter, CsrfFilter.class);
-        //rest of your code
-    }
+
 //rest of your code
 }
--- src/main/java/pl/coderslab/charityproject/WebSecurityConfig.java
+++ src/main/java/pl/coderslab/charityproject/WebSecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import pl.coderslab.charityproject.services.SpringDataUserDetailsService;
 
 @Configuration
@@ -42,6 +44,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
+
         http.
                 authorizeRequests()
                 .antMatchers("/", "/login", "/register").permitAll()
@@ -54,4 +61,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().logoutSuccessUrl("/");
     }
+
 }
