a2eb330868320ea05bf94120829d911a772512ab
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-16 21:26:29 +0100

    utf-8 antoher fix

--- src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
+++ src/main/java/pl/coderslab/charityproject/CharityProjectApplication.java
@@ -2,20 +2,10 @@ package pl.coderslab.charityproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleContextResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @SpringBootApplication
 public class CharityProjectApplication {
-//    @Bean(name="localeResolver")
-//    public LocaleContextResolver getLocaleContextResolver() {
-//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-//        localeResolver.setDefaultLocale(new Locale("pl","PL"));
-//        return localeResolver; }
+
 
     public static void main(String[] args) {
         SpringApplication.run(CharityProjectApplication.class, args);
