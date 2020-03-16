062ba4e58ef01e23156d0c841f07fc6272156136
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-16 20:48:22 +0100

    utf-8 antoher fix

--- src/main/java/pl/coderslab/charityproject/MvcConfig.java
+++ src/main/java/pl/coderslab/charityproject/MvcConfig.java
@@ -2,11 +2,13 @@ package pl.coderslab.charityproject;
 
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.Charset;
 import java.util.List;
 
+@Component
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
